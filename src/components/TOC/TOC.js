0c83960f18fdb596ab750f8bec6c70b9a18174f2
Danny Banks <djb@amazon.com>
2019-06-20 23:04:49 -0700

    Removing janky sreact scroll stuff and writing my own

--- src/components/TOC/TOC.js
+++ src/components/TOC/TOC.js
@@ -1,23 +1,69 @@
 import React from 'react'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
-import Scrollspy from 'react-scrollspy'
+import smoothScroll from './smoothScroll'
 
 import './TOC.css'
+
+
+
+class Scrollsy extends React.Component {
+  state = {
+    index: 0
+  }
+  
+  componentDidMount() {
+    this.offsets = this.props.items.map((link) => {
+      return document.getElementById(link.anchor).offsetTop;
+    });
+    
+    window.addEventListener('scroll', this.scroll);
+  }
+  
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.scroll);
+  }
+  
+  scroll = (e) => {
+    const bodyScroll = e.target.documentElement.scrollTop;
+    let index = 0;
+    
+    while (index <= this.offsets.length - 2) {
+      if (bodyScroll < this.offsets[index+1] - 100) {
+        break;
+      }
+      index++;
+    }
+    
+    // Don't cause unnecessary re-renders
+    if (this.state.index !== index) {
+      this.setState({ index });
+    }
+  }
+  
+  render() {
+    return (
+      <nav>
+        {this.props.items.map((item, i) => {
+          let className = 'toc-link '
+          className += i === this.state.index ? 'is-current' : '';
+          return (
+            <a key={item.label}
+              className={className}
+              href={`#${item.anchor}`}
+              onClick={() => smoothScroll.scrollTo(item.anchor)}>
+                {item.label}
+            </a>
+          )
+        })}
+      </nav>
+    )
+  }
+}
 
 export default ({ links }) => (
   <div className="toc">
     <div className="toc-inner">
       <h4>Table of contents</h4>
-      <Scrollspy items={ links.map((link) => link.anchor) } 
-        currentClassName="is-current">
-        {links.map((link) => (
-          <li key={link.anchor}>
-            <AnchorLink href={`#${link.anchor}`}>
-              {link.label}
-            </AnchorLink>
-          </li>
-        ))}
-      </Scrollspy>
+      <Scrollsy items={links} />
     </div>
   </div>
 )
