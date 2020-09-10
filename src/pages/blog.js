7eb4bca13d687d9b29afcc44322e835ef6a00e1d
Danny Banks <djb@amazon.com>
2020-09-10 14:41:45 -0700

    fixing some ish (#18)

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,67 +1,65 @@
 import React from 'react'
-import { Link, graphql } from 'gatsby'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 import SEO from '$components/SEO'
 import Post from '$components/Post'
 
-class BlogIndex extends React.Component {
-  render() {
-    const { data } = this.props;
-    const posts = data.allMdx.edges;
+const BlogIndex = () => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+        allMdx(
+          sort: { fields: [frontmatter___date], order: DESC },
+          filter: {
+            frontmatter: {
+              draft: {ne: true}
+            }
+          },
+        ) {
+          edges {
+            node {
+              excerpt(pruneLength: 200)
+              fields {
+                slug
+              }
+              frontmatter {
+                date(formatString: "MMMM DD, YYYY")
+                title
+                excerpt
+              }
+            }
+          }
+        }
+      }
+    `
+  )
+  const posts = data.allMdx.edges;
 
-    return (
-      <div className="content-inner">
-        <SEO
-          title="dbanksdesing - Blog"
-          keywords={[`blog`, `design systems`, `components`, `front-end`]}
-        />
-        <h1>Blog</h1>
-        <hr />
-        {posts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug
-          return (
-            <Post key={node.fields.slug}
-              href={node.fields.slug}
-              title={title}
-              date={node.frontmatter.date}
-              body={node.frontmatter.excerpt || node.excerpt} />
-          )
-        })}
-        <hr />
-      </div>
-    )
-  }
+  return (
+    <div className="content-inner">
+      <SEO
+        title="dbanksdesing - Blog"
+        keywords={[`blog`, `design systems`, `components`, `front-end`]}
+      />
+      <h1>Blog</h1>
+      <hr />
+      {posts.map(({ node }) => {
+        const title = node.frontmatter.title || node.fields.slug
+        return (
+          <Post key={node.fields.slug}
+            href={`/${node.fields.slug}`}
+            title={title}
+            date={node.frontmatter.date}
+            body={node.frontmatter.excerpt || node.excerpt} />
+        )
+      })}
+      <hr />
+    </div>
+  )
 }
 
 export default BlogIndex
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMdx(
-      sort: { fields: [frontmatter___date], order: DESC },
-      filter: {
-        frontmatter: {
-          draft: {ne: true}
-        }
-      },
-    ) {
-      edges {
-        node {
-          excerpt(pruneLength: 200)
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
-            excerpt
-          }
-        }
-      }
-    }
-  }
-`
